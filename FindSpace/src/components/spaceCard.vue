5deb754454733c3d28e6b1fe983181f9f7766486
pondc <chinnakrit.pond@gmail.com>
2021-04-29 15:48:27 +0700

    Add main pagw and fav page

---
+++ FindSpace/src/components/spaceCard.vue
@@ -0,0 +1,187 @@
+<template>
+  <IonCard
+    style="max-width:96%;margin-left:2%;background-color:white;border-radius:8px"
+    @click="showSpaceInfo(space.workspaceid)"
+  >
+    <div class="spacePic">
+      <!-- <img :src="require('@/assets/img/LoginImg.jpg')" /> -->
+      <img src="https://picsum.photos/700/300" />
+    </div>
+    <div class="spaceInfo">
+      <div class="spaceIcon">
+        <img src="https://picsum.photos/100" />
+      </div>
+      <div class="nameAndDistance">
+        <div class="spaceName">
+          {{ space.wsname }}
+        </div>
+        <div class="spaceDistance">
+          {{ kmFromSpace }}
+        </div>
+      </div>
+      <div class="spaceCrowd">
+        <ion-icon
+          class="personIcon"
+          :src="require('@/assets/icon/' + personIcon1)"
+        ></ion-icon>
+        <ion-icon
+          class="personIcon"
+          :src="require('@/assets/icon/' + personIcon2)"
+        ></ion-icon>
+        <ion-icon
+          class="personIcon"
+          :src="require('@/assets/icon/' + personIcon3)"
+        ></ion-icon>
+        <ion-icon
+          class="personIcon"
+          :src="require('@/assets/icon/' + personIcon4)"
+        ></ion-icon>
+      </div>
+    </div>
+    <div class="spaceFeed">
+      {{ feedbackText }}
+    </div>
+  </IonCard>
+</template>
+
+<script lang="ts">
+import {
+  // IonButton,
+  IonIcon,
+  IonCard,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "spaceCard",
+  components: {
+    // IonButton,
+    IonIcon,
+    IonCard,
+  },
+  props: ["space"],
+  beforeMount() {
+    const spaceInfo = this.$props.space;
+    this.createFeedBackMSG(spaceInfo);
+    this.createDistanceMSG(spaceInfo);
+    this.displayCrowdedNess(spaceInfo);
+  },
+  data() {
+    return {
+      personIcon1: "cPersonGreen.svg",
+      personIcon2: "cPersonLight.svg",
+      personIcon3: "cPersonOrange.svg",
+      personIcon4: "cPersonRed.svg",
+      feedbackText: "",
+      kmFromSpace: "",
+    };
+  },
+  methods: {
+    showSpaceInfo(id: number) {
+      // This one is for admin page
+      this.$router.push("/modify/" + id);
+
+      // This one is for user page
+      // this.$router.push("/SpaceInfo/" + id);
+    },
+    createFeedBackMSG(spaceInfo: any) {
+      if (spaceInfo.feedbackstatus) {
+        this.feedbackText =
+          new Date(spaceInfo.feedbacktime).toLocaleTimeString() + " No seats";
+      } else {
+        this.feedbackText =
+          new Date(spaceInfo.feedbacktime).toLocaleTimeString() +
+          " Seat(s) available";
+      }
+    },
+    createDistanceMSG(spaceInfo: any) {
+      this.kmFromSpace =
+        Number((spaceInfo.distance + "").split(".")[0]) / 1000 + " Km";
+    },
+    displayCrowdedNess(spaceInfo: any) {
+      console.log("hphphphphph");
+      console.log(spaceInfo);
+
+      if (spaceInfo.crowdednessstatus == 1) {
+        console.log("green");
+        this.personIcon1 = "cPersonGreen.svg";
+        this.personIcon2 = "cPerson.svg";
+        this.personIcon3 = "cPerson.svg";
+        this.personIcon4 = "cPerson.svg";
+      } else if (spaceInfo.crowdednessstatus == 2.4) {
+        this.personIcon1 = "cPersonGreen.svg";
+        this.personIcon2 = "cPersonLight.svg";
+        this.personIcon3 = "cPerson.svg";
+        this.personIcon4 = "cPerson.svg";
+        console.log("light");
+      } else if (spaceInfo.crowdednessstatus == 3.6) {
+        this.personIcon1 = "cPersonGreen.svg";
+        this.personIcon2 = "cPersonLight.svg";
+        this.personIcon3 = "cPersonOrange.svg";
+        this.personIcon4 = "cPerson.svg";
+        console.log("orange");
+      } else if (spaceInfo.crowdednessstatus == 5) {
+        this.personIcon1 = "cPersonGreen.svg";
+        this.personIcon2 = "cPersonLight.svg";
+        this.personIcon3 = "cPersonOrange.svg";
+        this.personIcon4 = "cPersonRed.svg";
+        console.log("red");
+      }
+    },
+  },
+});
+</script>
+<style lang="css" scoped>
+.spaceList {
+  height: 80%;
+  padding-top: 8px;
+}
+.spaceCard {
+  border-radius: 12px;
+  background-color: white;
+  max-width: 96%;
+  height: 176px;
+  margin-left: 2%;
+}
+.spacePic {
+  border-top-left-radius: 12px;
+  border-top-right-radius: 12px;
+  height: 104px;
+  overflow: hidden;
+}
+.spaceInfo {
+  height: 48px;
+  display: flex;
+}
+.spaceIcon {
+  max-width: 48px;
+}
+.nameAndDistance {
+  padding-left: 4px;
+  max-width: 144px;
+  display: flex;
+  flex-direction: column;
+}
+.spaceName {
+  overflow: hidden;
+  height: 50%;
+  color: #4a4d3e;
+}
+.spaceDistance {
+  overflow: hidden;
+  height: 50%;
+  color: #4a4d3e;
+}
+.spaceCrowd {
+  max-width: 36%;
+  padding: 14px 0;
+}
+.spaceFeed {
+  border-bottom-left-radius: 12px;
+  border-bottom-right-radius: 12px;
+  height: 24px;
+}
+.personIcon {
+  font-size: 24px;
+}
+</style>
--- FindSpace/src/views/Tab1.vue
+++ FindSpace/src/views/Tab1.vue
@@ -1,68 +1,100 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-button @click="reload()">
-        reload
-      </ion-button>
-    </ion-content>
+    <div class="mainDiv">
+      <img class="nearbyBanner" :src="require('@/assets/img/FavSpace.png')" />
+      <div class="searchBox">search box</div>
+      <div class="spaceList">
+        <div v-for="space in spaces" :key="space.workspaceid">
+          <spaceList :space="space"></spaceList>
+        </div>
+      </div>
+    </div>
   </ion-page>
 </template>
 
 <script lang="ts">
 import {
   IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonButton,
+  // IonIcon,
 } from "@ionic/vue";
 import { defineComponent } from "vue";
 import axios from "axios";
+import spaceList from "../components/spaceList.vue";
 
 export default defineComponent({
-  // name: 'Tab1',
-  name: "Nearby",
+  name: "Tab2",
   components: {
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
     IonPage,
-    IonButton,
+    spaceList,
+    // IonIcon,
   },
   beforeMount() {
-    console.log("hello from tab 1");
-    this.helloBackend();
+    console.log("hello from tab 2");
+    this.watchLocation();
+    // this.getNearbySpaceList();
+  },
+  data() {
+    return {
+      searchWord: "",
+      spaces: [],
+      location: {
+        lat: 0,
+        long: 0,
+      },
+    };
   },
   methods: {
-    helloBackend() {
-      // axios.defaults.withCredentials = true;
-      return (
-        axios
-          // .get("http://localhost:5678/cumap")
-          .get("http://localhost:5678/realtime/recommWS")
-          .then((res) => {
-            console.log(res.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          })
-      );
-    },
     reload() {
       window.location.reload();
+    },
+    watchLocation() {
+      const geo = navigator.geolocation;
+      geo.watchPosition((res) => {
+        console.log("watching location....");
+        console.log(res);
+        this.updateLocation(res);
+        this.getNearbySpaceList();
+      });
+    },
+    updateLocation(data: any) {
+      this.location.lat = data.coords.latitude;
+      this.location.long = data.coords.longitude;
+      this.getNearbySpaceList();
+    },
+    getNearbySpaceList() {
+      const endPointURL = "http://localhost:5678/admin/workspace";
+      // const endPointURL =
+      //   "http://localhost:5678/homepage/recommWS?Lat=" +
+      //   userLat +
+      //   "&Long=" +
+      //   userLong;
+      return axios
+        .get(endPointURL)
+        .then((res) => {
+          this.spaces = res.data.rows;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
   },
 });
 </script>
+<style lang="css" scoped>
+.mainDiv {
+  background-color: #faf6f0;
+  color: black;
+  height: 100%;
+}
+.nearbyBanner {
+  width: 100%;
+}
+.searchBox {
+  height: 10%;
+  background-color: aqua;
+}
+.spaceList {
+  height: 80%;
+  padding-top: 8px;
+}
+</style>
